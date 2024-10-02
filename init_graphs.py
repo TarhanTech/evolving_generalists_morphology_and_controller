6a6bc7907d7cd19cfc3d5e72979b4f02eb2a6227
TarhanTech <tarhantech@gmail.com>
2024-10-02 16:15:55 +0200

    Add decoding to graph generation for graph representation

--- init_graphs.py
+++ init_graphs.py
@@ -1,5 +1,26 @@
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+import torch
+from torch import Tensor
+from source.mj_env import Morphology
+def _decode_morph_params(morph_params: Tensor) -> Tensor:
+    length_params, width_params = torch.split(
+        morph_params, (Morphology.total_leg_length_params, Morphology.total_leg_width_params)
+    )
+
+    a: float = (Morphology.leg_length_range[1] - Morphology.leg_length_range[0]) / (
+        Morphology.morph_params_bounds_enc[1] - Morphology.morph_params_bounds_enc[0]
+    )
+    b: float = Morphology.leg_length_range[0] - (a * Morphology.morph_params_bounds_enc[0])
+    decoded_leg_length: Tensor = (a * length_params) + b
+
+    c: float = (Morphology.leg_width_range[1] - Morphology.leg_width_range[0]) / (
+        Morphology.morph_params_bounds_enc[1] - Morphology.morph_params_bounds_enc[0]
+    )
+    d: float = Morphology.leg_width_range[0] - (c * Morphology.morph_params_bounds_enc[0])
+    decoded_leg_width: Tensor = (c * width_params) + d
+
+    return torch.cat((decoded_leg_length, decoded_leg_width), dim=0)
 
 df = pd.read_csv('./gen_score_pandas_df.csv')
 
