499cfbc77ecb759312e15f64400bef04a9f588be
Kubilay <tarhantech@gmail.com>
2024-10-04 15:05:16 +0200

    chagne to pdf

--- env_images.py
+++ env_images.py
@@ -18,12 +18,12 @@ os.makedirs(f"{folder_run_data}/hills", exist_ok=True)
 for training_env in tr_schedule.training_schedule:
     if isinstance(training_env, RoughTerrain):
         ind.setup_ant_rough(params, training_env.floor_height, training_env.block_size)
-        ind.make_screenshot_env(f"{folder_run_data}/rough/rough_{training_env.block_size}_{training_env.floor_height}.png")
+        ind.make_screenshot_env(f"{folder_run_data}/rough/rough_{training_env.block_size}_{training_env.floor_height}.pdf")
     elif isinstance(training_env, HillsTerrain):
         ind.setup_ant_hills(params, training_env.floor_height, training_env.scale)
-        ind.make_screenshot_env(f"{folder_run_data}/hills/hills_{training_env.scale}_{training_env.floor_height}.png")
+        ind.make_screenshot_env(f"{folder_run_data}/hills/hills_{training_env.scale}_{training_env.floor_height}.pdf")
     elif isinstance(training_env, DefaultTerrain):
         ind.setup_ant_default(params)
-        ind.make_screenshot_env(f"{folder_run_data}/default_terrain.png")
+        ind.make_screenshot_env(f"{folder_run_data}/default_terrain.pdf")
     else:
         assert False, "Class type not supported"
